1818c4562d3608ac9b32b43e8388df0df531c8cd
ipungg-junior <ipungg.id@gmail.com>
2024-11-07 16:21:48 +0700

    Rev GET handler dashboard, migrate to class method Visitor to counting visitor

--- viewer/dashboard.py
+++ viewer/dashboard.py
@@ -8,14 +8,10 @@ from services.firebase import firebase_upload, firebase_delete
 from services.addons import analyze_storage
 from services.notification import system_notification
 from apps.models import Partner, Article, Visitor
-from django.utils import timezone
-from datetime import timedelta
 
 
 # Logger information object
 from services.logger import logger
-
-
 
 
 @method_decorator(role_required(['root', 'admin', 'staff']), name='dispatch')
@@ -26,39 +22,14 @@ class Dashboard(View):
 
         if (self.context == 'main-dashboard'):
             total_ , used_, free_ = analyze_storage()
+            
             ctx = {}
-            now = timezone.now()
-            # Pengunjung harian
-            start_of_today = now.replace(hour=0, minute=0, second=0)
-            daily_visitors = Visitor.objects.filter(visited_at__gte=start_of_today).count()
-            
-            start_of_week = now - timedelta(days=7)
-            weekly_visitors = Visitor.objects.filter(visited_at__gte=start_of_week)
-            weekly_visitors_count = weekly_visitors.count()
-            
-            start_of_month = now - timedelta(days=30)
-            monthly_visitors = Visitor.objects.filter(visited_at__gte=start_of_month)
-            monthly_visitors_count = monthly_visitors.count()
-
-            visitor_weekly_graph = []
-
-            for day in range(7):
-                date = now + timedelta(days=day)
-                visitor_count = Visitor.objects.filter(visited_at__date=date).count()  # Hitung pengunjung per hari
-                visitor_weekly_graph.append({
-                    'date': date.strftime('%Y-%m-%d'),
-                    'count': visitor_count
-                })            
-            
-            most_viewed = most_viewed_article()
-            ctx['articles'] = most_viewed
-            ctx['daily_visit'] = daily_visitors
-            ctx['weekly_visit'] = weekly_visitors_count
-            ctx['monthly_visit'] = monthly_visitors_count
+            ctx['weekly_visit'] = Visitor.get_weekly_count()
+            ctx['notifications'] = system_notification()
             ctx['total'] = total_
             ctx['system'] = used_
             ctx['free'] = free_
-            ctx['notifications'] = system_notification()
+        
             # Konversi GB ke MB
             total_size_mb = float(total_) * 1024
             # Hitung persentase
