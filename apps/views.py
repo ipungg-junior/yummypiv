1527e30c78d193806d54e41c40dbdfa0a818c199
yummypiv.cloud <ipungg.id@gmail.com>
2025-11-03 04:11:45 +0000

    Rev typo quotestring

--- apps/views.py
+++ apps/views.py
@@ -32,7 +32,7 @@ def landing(request):
     resp.headers['Cache-Control'] = "no-cache, no-store, must-revalidate"
     resp.headers['Pragma'] = "no-cache"
     resp.headers['Expires'] = "0"
-    logger.info(f'{request.META.get('REMOTE_ADDR')} - {request.META.get('HTTP_USER_AGENT')} - Landing page')
+    logger.info(f'{request.META.get("REMOTE_ADDR")} - {request.META.get("HTTP_USER_AGENT")} - Landing page')
     return resp
 
 def landing_catering(request):
@@ -40,7 +40,7 @@ def landing_catering(request):
     resp.headers['Cache-Control'] = "no-cache, no-store, must-revalidate"
     resp.headers['Pragma'] = "no-cache"
     resp.headers['Expires'] = "0"
-    logger.info(f'{request.META.get('REMOTE_ADDR')} - {request.META.get('HTTP_USER_AGENT')} - Landing Catering page')
+    logger.info(f'{request.META.get("REMOTE_ADDR")} - {request.META.get("HTTP_USER_AGENT")} - Landing Catering page')
     return resp
 
 def services(request, name):
@@ -58,7 +58,7 @@ def services(request, name):
     resp.headers['Cache-Control'] = "no-cache, no-store, must-revalidate"
     resp.headers['Pragma'] = "no-cache"
     resp.headers['Expires'] = "0"
-    logger.info(f'{request.META.get('REMOTE_ADDR')} - {request.META.get('HTTP_USER_AGENT')} - Service {my_template} page landing')
+    logger.info(f'{request.META.get("REMOTE_ADDR")} - {request.META.get("HTTP_USER_AGENT")} - Service {my_template} page landing')
     return resp
 
 def about(request):
@@ -76,5 +76,5 @@ def about(request):
     resp.headers['Cache-Control'] = "no-cache, no-store, must-revalidate"
     resp.headers['Pragma'] = "no-cache"
     resp.headers['Expires'] = "0"
-    logger.info(f'{request.META.get('REMOTE_ADDR')} - {request.META.get('HTTP_USER_AGENT')} - About page')
+    logger.info(f'{request.META.get("REMOTE_ADDR")} - {request.META.get("HTTP_USER_AGENT")} - About page')
     return resp
